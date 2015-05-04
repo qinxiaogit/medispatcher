7b349bac279cf36f98ba0f651754263b0ef8aa15
chaos <suchaoabc@163.com>
2015-05-04 12:05:54 +0800

    update tests

--- data/serializer_test.go
+++ data/serializer_test.go
@@ -3,6 +3,7 @@ package data
 import (
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"testing"
 	"time"
 )
@@ -11,10 +12,10 @@ func TestSerializer(t *testing.T) {
 	msg := MessageStuct{
 		MsgKey: "test",
 		Body: map[interface{}]interface{}{
-			"a":   1,
-			"b":   "fsdfsf",
+			"a": 1,
+			"b": "fsdfsf",
 			123: "fff",
-			"e":   map[interface{}]interface{}{"a1": 12323},
+			"e": map[interface{}]interface{}{"a1": 12323},
 			"ll": []interface{}{
 				map[interface{}]interface{}{
 					"a":  123,
@@ -26,7 +27,6 @@ func TestSerializer(t *testing.T) {
 		Time:        float64(time.Now().UnixNano()) * 1E-9,
 		OriginJobId: uint64(9898989),
 	}
-
 
 	var dMsg MessageStuct
 	b, err := SerializeMessage(msg)
@@ -41,13 +41,13 @@ func TestSerializer(t *testing.T) {
 		t.Log(dMsg)
 	}
 	mB, err := json.Marshal(dMsg.Body)
-	if err != nil{
+	if err != nil {
 		t.Error(err)
 		t.Fail()
 	} else {
 		var mBi interface{}
-		err=json.Unmarshal(mB, &mBi)
-		if err != nil{
+		err = json.Unmarshal(mB, &mBi)
+		if err != nil {
 			t.Log(err)
 			t.Fail()
 		} else {
@@ -55,6 +55,27 @@ func TestSerializer(t *testing.T) {
 			t.Log(fmt.Sprintf("%+v", mBi))
 		}
 	}
-
 }
 
+func TestMsgackUnserialization(t *testing.T) {
+	s, err := ioutil.ReadFile("serializer_test.data")
+	if err != nil {
+		t.Error(err)
+		t.Fail()
+		return
+	}
+	msg, err := UnserializeMessage([]byte(s))
+	if err != nil {
+		t.Error(err)
+		t.Fail()
+	} else {
+		t.Logf("Unserialized as: %+v", msg)
+		jB, err := json.Marshal(msg)
+		if err != nil {
+			t.Error(err)
+			t.Fail()
+		} else {
+			t.Logf("json string: %s", jB)
+		}
+	}
+}
