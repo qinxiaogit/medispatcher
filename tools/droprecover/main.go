0961936c6aeb11c4ce95336647a375e75339174d
iampastor <pastor.xin@gmail.com>
2019-03-09 02:11:22 +0800

    增加消息回复工具

---
+++ tools/droprecover/main.go
@@ -0,0 +1,270 @@
+package main
+
+import (
+	"bufio"
+	"encoding/json"
+	"errors"
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"medispatcher/broker/beanstalk"
+	"medispatcher/data"
+	"medispatcher/osutil"
+	"os"
+	"os/signal"
+	"path/filepath"
+	"sort"
+	"strconv"
+	"strings"
+	"sync"
+	"syscall"
+	"time"
+)
+
+const (
+	prefix = "subscription"
+)
+
+var (
+	dataDir string
+	subId   int
+)
+
+func init() {
+	flag.StringVar(&dataDir, "d", "", "drop message log dir")
+	flag.IntVar(&subId, "i", 0, "subscription id")
+	flag.Parse()
+}
+
+func main() {
+	var exitChan = make(chan struct{})
+	var wg sync.WaitGroup
+	err := os.Chdir(dataDir)
+	if err != nil {
+		log.Fatalf("chdir %s: %s", dataDir, err.Error())
+	}
+
+	if subId != 0 {
+		wg.Add(1)
+		go func() {
+			recoverDropMessage(exitChan, int32(subId))
+			wg.Done()
+		}()
+	}
+
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, syscall.SIGHUP, syscall.SIGQUIT, syscall.SIGTERM, syscall.SIGINT, syscall.SIGSTOP)
+	for {
+		s := <-c
+		log.Printf("get a signal %s", s.String())
+		switch s {
+		case syscall.SIGQUIT, syscall.SIGTERM, syscall.SIGSTOP, syscall.SIGINT:
+			close(exitChan)
+			wg.Wait()
+			return
+		}
+	}
+}
+
+func recoverDropMessage(exitChan chan struct{}, subId int32) {
+	metaData, err := getMetaData(subId)
+	if err != nil {
+		log.Fatalf("get subscription [%d] metadata: %s", subId, err.Error())
+	}
+
+	files, err := listRecoverFiles(subId, metaData)
+	if err != nil {
+		log.Fatalf("list subscription [%d] recover files: %s", subId, err.Error())
+	}
+	for _, file := range files {
+		log.Printf("found recover file: %s", file)
+	}
+
+	for _, file := range files {
+		select {
+		case <-exitChan:
+			log.Printf("stop recover files")
+			return
+		default:
+			metaData.filename = file
+			log.Printf("recover file %s:%d", metaData.filename, metaData.position)
+			err := recoverFile(exitChan, file, metaData)
+			if err != nil {
+				log.Fatalf("recover file %s: %s", file, err.Error())
+			}
+			metaData.position = 0
+		}
+	}
+}
+
+func getMetaData(subId int32) (*MetaData, error) {
+	metaData := &MetaData{}
+	mfileName := fmt.Sprintf("%s_%d.metadata", prefix, subId)
+	if !osutil.FileExists(mfileName) {
+		log.Printf("no metadata found")
+		f, err := os.Create(mfileName)
+		if err != nil {
+			return metaData, err
+		}
+		metaData.f = f
+		return metaData, nil
+	}
+	f, err := os.OpenFile(mfileName, os.O_RDWR, 0666)
+	if err != nil {
+		return nil, fmt.Errorf("open metadata file %s: %s", mfileName, err.Error())
+	}
+	metaData.f = f
+	err = metaData.read()
+	log.Printf("found metadata %s:%d", metaData.filename, metaData.position)
+	return metaData, err
+}
+
+func listRecoverFiles(subId int32, m *MetaData) ([]string, error) {
+	f, err := os.Open(".")
+	if err != nil {
+		return nil, err
+	}
+	list, err := f.Readdir(-1)
+	f.Close()
+	if err != nil {
+		return nil, err
+	}
+	sort.Slice(list, func(i, j int) bool { return list[i].Name() < list[j].Name() })
+	var files []string
+	for _, fi := range list {
+		match, err := filepath.Match(fmt.Sprintf("%s_%d_*.data", prefix, subId), fi.Name())
+		if err != nil {
+			return nil, fmt.Errorf("match subscription [%d]: %s", subId, err.Error())
+		}
+		if match && fi.Name() >= m.filename {
+			files = append(files, fi.Name())
+		}
+	}
+	return files, nil
+}
+
+// subscription_1_1552037666.data
+// subscription_1.metadata
+func decodeFilename(fname string) (prefix string, subId int32, ts int64, err error) {
+	fss := strings.Split(fname, ".")
+	if len(fss) != 2 {
+		err = errors.New("bad filename")
+		return
+	}
+	ss := strings.Split(fss[0], "_")
+	if len(ss) < 2 {
+		err = errors.New("bad filename")
+	}
+
+	prefix = ss[0]
+	subId64, err := strconv.ParseInt(ss[1], 10, 32)
+	if err != nil {
+		return
+	}
+	subId = int32(subId64)
+
+	if len(ss) == 3 {
+		ts, err = strconv.ParseInt(ss[2], 10, 64)
+	}
+	return
+}
+
+func recoverFile(exitChan chan struct{}, fname string, metaData *MetaData) error {
+	f, err := os.Open(fname)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+	_, err = f.Seek(metaData.position, 0)
+	if err != nil {
+		return err
+	}
+
+	r := bufio.NewReader(f)
+	for {
+		select {
+		case <-exitChan:
+			log.Printf("stop recover file %s:%d", fname, metaData.position)
+			return nil
+		default:
+			rawData, err := r.ReadBytes('\n')
+			if err != nil {
+				if err == io.EOF {
+					return nil
+				} else {
+					return err
+				}
+			}
+			msg, err := decodeMessage(rawData)
+			if err != nil {
+				return fmt.Errorf("decode message: %s", err.Error())
+			}
+			err = handleMessageStruct(msg)
+			if err != nil {
+				return fmt.Errorf("handle message: %s", err.Error())
+			}
+			metaData.position += int64(len(rawData))
+			if err := metaData.write(); err != nil {
+				return fmt.Errorf("write metadata: %s", err.Error())
+			}
+		}
+	}
+}
+
+func decodeMessage(rawData []byte) (*data.MessageStuct, error) {
+	var msgR beanstalk.Msg
+	err := json.Unmarshal(rawData[:len(rawData)-1], &msgR)
+	if err != nil {
+		return nil, err
+	}
+
+	return data.UnserializeMessage(msgR.Body)
+}
+
+func handleMessageStruct(msg *data.MessageStuct) error {
+	//TODO:恢复消息到队列
+	// log.Printf("handle message: %+v", msg)
+	time.Sleep(time.Second * 1)
+	return nil
+}
+
+type MetaData struct {
+	filename string
+	position int64
+	f        *os.File
+}
+
+func (m *MetaData) write() error {
+	err := m.f.Truncate(0)
+	if err != nil {
+		return err
+	}
+	_, err = m.f.Seek(0, 0)
+	if err != nil {
+		return err
+	}
+	_, err = m.f.Write([]byte(fmt.Sprintf("%s\n%d\n", m.filename, m.position)))
+	return err
+}
+
+func (m *MetaData) read() error {
+	r := bufio.NewReader(m.f)
+	fname, err := r.ReadString('\n')
+	if err != nil {
+		return fmt.Errorf("read metadata: %s", err.Error())
+	}
+
+	position, err := r.ReadString('\n')
+	if err != nil {
+		return fmt.Errorf("read metadata: %s", err.Error())
+	}
+
+	pos, err := strconv.ParseInt(position[:len(position)-1], 10, 64)
+	if err != nil {
+		return fmt.Errorf("read metadata: %s", err.Error())
+	}
+	m.filename = fname[:len(fname)-1]
+	m.position = pos
+	return nil
+}
