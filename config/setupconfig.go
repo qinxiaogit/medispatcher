faf97e8a957345ba52f2b7efe5d6d7e3b1ef2395
chaos <suchaoabc@163.com>
2019-04-11 15:22:31 +0800

    update version number

--- config/setupconfig.go
+++ config/setupconfig.go
@@ -16,7 +16,7 @@ var BuildTime = "unknonw"
 var GitHash = ""
 
 const (
-	VerNo           = "2.4.4"
+	VerNo           = "2.4.5"
 	MAX_CONNECTIONS = 15000
 	// 客户端响应超时
 	CLIENT_TIMEOUT  = time.Second * 5
